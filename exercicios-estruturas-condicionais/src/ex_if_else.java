8b3be4e6068fb9274316e6f503f4502936c93d0f
Jonathas Chauvin <jonathaschauvin@gmail.com>
2026-02-02 23:59:40 -0300

    exercício if/else - estruturas condicionais

---
+++ exercicios-estruturas-condicionais/src/ex_if_else.java
@@ -0,0 +1,13 @@
+
+public class ex_if_else {
+	public static void main(String[] args) {
+        int numero = 10;
+
+        if (numero > 0) {
+            System.out.println("Número positivo");
+        } else {
+            System.out.println("Número negativo ou zero");
+        }
+    }
+
+}

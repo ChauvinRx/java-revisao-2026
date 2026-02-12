26bba8ebe621ebfbcfe6aaff7f02d8a9a557451c
Jonathas Chauvin <jonathaschauvin@gmail.com>
2026-02-12 00:03:03 -0300

    enunciado atualizado

--- exercicios-estruturas-condicionais/src/ex_if_else07.java
+++ exercicios-estruturas-condicionais/src/ex_if_else07.java
@@ -8,8 +8,14 @@ public class ex_if_else07 {
 		 Locale.setDefault(Locale.US);
 	        Scanner sc = new Scanner(System.in);
 	        
-	        System.out.println("Digite os dois valores para analisar no plano");
-	        System.out.println("(Enunciado no documento)");
+	        System.out.println("Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas\r\n"
+	        		+ "de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o\r\n"
+	        		+ "ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).\r\n"
+	        		+ "Se o ponto estiver na origem, escreva a mensagem “Origem”.\r\n"
+	        		+ "Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a\r\n"
+	        		+ "situação.\n");
+	        
+	        System.out.println("Digite os dois valores para analisar no plano: ");
 
 	        double x = sc.nextDouble();
 	        double y = sc.nextDouble();
